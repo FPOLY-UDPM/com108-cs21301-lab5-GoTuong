1f5d4e3c39843acb6dd373da9482c8a178e3a9cf
GoTuong <tuongtung2018@gmail.com>
2026-02-03 08:44:29 +0700

    Nộp lab 5 bài 1 bài 3

--- bai1.c
+++ bai1.c
@@ -1,29 +1,50 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Lê Minh Tường]
+ * MSSV :      [PS37909]
+ * Lớp:       [CS21301]
  *****************************************************************************/
 
-//  BÀI 1: XÂY DỰNG CHƯƠNG TRÌNH TÌM GIÁ TRỊ LỚN NHẤT TRONG 3 SỐ  
-//  Input: Nhập từ bàn phím 3 số bất kỳ 
-//  Output: Xuất ra màn hình số lớn nhất trong 3 số 
-//  Hướng dẫn: Xây dựng hàm tìm giá trị lớn nhất trong 3 số. Hàm có giá trị trả về mà 
-//  giá trị lớn nhất giữa 3 số 
+//  BÀI 1: XÂY DỰNG CHƯƠNG TRÌNH TÌM GIÁ TRỊ LỚN NHẤT TRONG 3 SỐ
+//  Input: Nhập từ bàn phím 3 số bất kỳ
+//  Output: Xuất ra màn hình số lớn nhất trong 3 số
+//  Hướng dẫn: Xây dựng hàm tìm giá trị lớn nhất trong 3 số. Hàm có giá trị trả về mà
+//  giá trị lớn nhất giữa 3 số
 
 #include <stdio.h>
 
-// //Tạo hàm 
-// int tenHam(so1, so2, so3){ 
-// int max; 
-// //Cách giải để tìm ra số lớn nhất 
-// return max; 
-// } 
+// //Tạo hàm
+// int tenHam(so1, so2, so3){
+// int max;
+// //Cách giải để tìm ra số lớn nhất
+// return max;
+// }
 
-int main() {
+int soMax(int a, int b, int c)
+{
+    if (a >= b && a >= c)
+    {
+        return a;
+    }
+    else if (b >= a && b >= c)
+    {
+        return b;
+    }
+    else
+    {
+        return c;
+    }
+}
 
-    // //Gọi hàm trong hàm main 
-    // //ví dụ 
+int main()
+{
+
+    // //Gọi hàm trong hàm main
+    // //ví dụ
     // int soMax = temHam(3,5,10)
-    
+    int max;
+
+    max = soMax(5, 6, 7);
+    printf("Số lớn nhất trong 3 số là: %d", max);
+
     return 0;
 }
--- bai3.c
+++ bai3.c
@@ -1,23 +1,37 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Lê Minh Tường]
+ * MSSV :      [PS37909]
+ * Lớp:       [CS21301]
  *****************************************************************************/
 
-//  BÀI 3: XÂY DỰNG HÀM HOÁN VỊ 
-//  Input: Nhập vào từ bàn phím 2 giá trị a,b 
-//  Output: hoán vị 2 giá trị của a và b 
-//  Hướng dẫn thực hiện: 
-//  Xây dựng hàm hoán vị sử dụng con trỏ và gọi hàm theo tham chiếu 
+//  BÀI 3: XÂY DỰNG HÀM HOÁN VỊ
+//  Input: Nhập vào từ bàn phím 2 giá trị a,b
+//  Output: hoán vị 2 giá trị của a và b
+//  Hướng dẫn thực hiện:
+//  Xây dựng hàm hoán vị sử dụng con trỏ và gọi hàm theo tham chiếu
 
 #include <stdio.h>
 
-//Tạo hàm 
-
-int main() {
-
-    // //Gọi hàm trong hàm main 
-    
-    return 0;
+// Tạo hàm
+void hamSwap(int *a, int *b)
+{
+    int thung = *a;
+    *a = *b;
+    *b = thung;
 }
 
+int main()
+{
+    int a, b;
+
+    printf("Nhập a: ");
+    scanf("%d", &a);
+
+    printf("\nNhập b: ");
+    scanf("%d", &b);
+    // //Gọi hàm trong hàm main
+    hamSwap(&a, &b);
+    printf("\nSau hoán vị a = %d", a);
+    printf("\nSau hoán vị b = %d", b);
+    return 0;
+}
